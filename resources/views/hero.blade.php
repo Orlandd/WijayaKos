a46ef90b9cacd5913db380efbef7a164b6448e45
Orlandd <alvenorlando@gmail.com>
2024-06-14 11:47:22 +0700

    update: hero

--- resources/views/hero.blade.php
+++ resources/views/hero.blade.php
@@ -5,31 +5,36 @@
         <div class=" flex flex-wrap mx-auto md:flex-nowrap gap-1 h-full mt-10">
 
             <div class="w-full flex gap-1 md:w-1/2">
-                <div
-                    class="bg-no-repeat bg-center bg-cover md:hidden mx-auto w-[400px] h-[300px] md:w-[300px] md:h-[608px] bg-slate-500 rounded-xl overflow-hidden">
+                <div class="bg-no-repeat bg-center bg-cover md:hidden mx-auto w-[400px] h-[300px] md:w-[300px] md:h-[608px] bg-slate-500 rounded-xl overflow-hidden"
+                    style="background-image: url(/storage/hero/4.jpg)">
                 </div>
                 <div class=" grid-cols-2 grid-flow-row gap-4 w-full hidden md:grid">
 
                     <div class="row-span-2">
-                        <div
-                            class="bg-no-repeat bg-center bg-cover hidden mx-auto w-[400px] h-[300px] md:w-full md:h-[416px] md:inline-block bg-slate-500 rounded-xl overflow-hidden">
+                        <div class="bg-no-repeat bg-center bg-cover hidden mx-auto w-[400px] h-[300px] md:w-full md:h-[416px] md:inline-block bg-slate-500 rounded-xl overflow-hidden"
+                            style="background-image: url(/storage/hero/1.jpg)">
+                            {{-- <img src="/storage/hero/1.jpg" alt=""> --}}
                         </div>
                     </div>
                     <div>
-                        <div
-                            class="bg-no-repeat bg-center bg-cover	hidden mx-auto w-[400px] h-[300px] md:w-full  md:max-h-[200px] md:inline-block bg-slate-500 rounded-xl overflow-hidden">
+                        <div class="bg-no-repeat bg-center bg-cover	hidden mx-auto w-[400px] h-[300px] md:w-full  md:max-h-[200px] md:inline-block bg-slate-500 rounded-xl overflow-hidden"
+                            style="background-image: url(/storage/hero/2.jpg)">
+                            {{-- <img src="/storage/hero/2.jpg" alt=""> --}}
                         </div>
                     </div>
                     <!-- ... -->
                     <div>
-                        <div
-                            class="bg-no-repeat bg-center bg-cover	hidden mx-auto w-[400px] h-[300px] md:w-full md:max-h-[200px] md:inline-block bg-slate-500 rounded-xl overflow-hidden">
+                        <div class="bg-no-repeat bg-center bg-cover 	hidden mx-auto w-[400px] h-[300px] md:w-full md:max-h-[200px] md:inline-block bg-slate-500 rounded-xl overflow-hidden"
+                            style="background-image: url(/storage/hero/3.jpg)">
+                            {{-- <img src="/storage/hero/3.jpg" alt=""> --}}
+
                         </div>
                     </div>
                     <div class="col-span-2">
-                        <div
-                            class="bg-no-repeat bg-center bg-cover	hidden mx-auto w-[400px] h-[300px] md:w-full md:max-h-[200px] md:inline-block bg-slate-500 rounded-xl overflow-hidden">
+                        <div class="bg-no-repeat bg-center bg-cover	hidden mx-auto w-[400px] h-[300px] md:w-full md:max-h-[200px] md:inline-block bg-slate-500 rounded-xl overflow-hidden"
+                            style="background-image: url(/storage/hero/4.jpg)">
                         </div>
+
                     </div>
 
                 </div>
